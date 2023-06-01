ccfa404e74040865e82a3e7eb26737457d030a4f
tcourtis <toby.courtis@hotmail.com>
2023-06-01 18:43:38 +0100

    ✨ Hackerrank bill division [easy]

---
+++ hackerrank/easy/bill-division/A.py
@@ -0,0 +1,40 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'bonAppetit' function below.
+#
+# The function accepts following parameters:
+#  1. INTEGER_ARRAY bill
+#  2. INTEGER k
+#  3. INTEGER b
+#
+
+def bonAppetit(bill, k, b):
+    # Write your code here
+    bill.pop(k)
+    adjusted_total = sum(bill)
+    split_adj_total = int(adjusted_total / 2)
+    if split_adj_total != b:
+        print(b - split_adj_total)
+    else:
+        print('Bon Appetit')
+
+
+if __name__ == '__main__':
+    first_multiple_input = input().rstrip().split()
+
+    n = int(first_multiple_input[0])
+
+    k = int(first_multiple_input[1])
+
+    bill = list(map(int, input().rstrip().split()))
+
+    b = int(input().strip())
+
+    bonAppetit(bill, k, b)
