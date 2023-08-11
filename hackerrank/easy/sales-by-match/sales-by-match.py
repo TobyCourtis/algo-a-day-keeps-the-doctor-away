6f0c7093f1cf2dc91e6cfcde3c38a9fb1862c46f
tcourtis <toby.courtis@hotmail.com>
2023-08-11 12:44:28 +0100

    ✨ Sales by match easy problem

---
+++ hackerrank/easy/sales-by-match/sales-by-match.py
@@ -0,0 +1,39 @@
+import math
+import os
+
+
+#
+# Complete the 'sockMerchant' function below.
+#
+# The function is expected to return an INTEGER.
+# The function accepts following parameters:
+#  1. INTEGER n
+#  2. INTEGER_ARRAY ar
+#
+
+def sockMerchant(n, ar):
+    # Write your code here
+    distinct = []
+    for i in ar:
+        if i not in distinct:
+            distinct.append(i)
+
+    pairs = 0
+    for num in distinct:
+        count = ar.count(num)
+        pairs += math.floor(count / 2)
+    return pairs
+
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input().strip())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = sockMerchant(n, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
