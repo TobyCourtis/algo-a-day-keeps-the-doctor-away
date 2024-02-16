2b03d1bc6ec8fbaeec57564b902414c63c8f626b
tobycourtis <toby.courtis@hotmail.com>
2024-02-16 12:51:32 +0000

    📄 Update docstring on mock test

---
+++ hackerrank/mock_assessment/citadel/main.py
@@ -0,0 +1,9 @@
+"""
+Construct a quirky binary search tree
+
+
+QST is a binary search tree which allows for duplicate value nodes i.e multiple nodes with same value
+
+- algorithm is given, just implement
+- Very easy
+"""
