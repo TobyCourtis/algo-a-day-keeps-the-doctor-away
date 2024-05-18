b70230297c8e38acd2fd6d765f92650667e71526
tobycourtis <toby.courtis@hotmail.com>
2024-05-18 14:17:06 +0100

    ✨ Practice leetcode - blind 75 problem 60 w/comments

--- leetcode/practice-blind75/60-missing-number.py
+++ leetcode/practice-blind75/60-missing-number.py
@@ -14,3 +14,24 @@ class Solution:
 
 s = Solution()
 print(s.missingNumber([0, 1, 3]))
+
+
+class Solution2:
+    def missingNumber(self, nums: List[int]) -> int:
+        res = len(nums)  # len(nums) is always largest value in the list e.g [0,1,3] len == 3 which is largest.
+
+        # if nums is 0 -> 9 with one number missing then arr length = 9
+        # let's begin with res as the largest number (9)
+        # if 9 is actually missing then at the end we'll return res which is 9
+        # if 8 is actually missing then at one point we'll have:
+        # res = 9
+        # res += 8
+        # res -= 9
+        # res = 8   <-- this num is returned
+
+        # think of this approach as building a sum whilst taking away the actual values
+        for i in range(len(nums)):
+            res += i  # calculate sum of all nums
+            res -= nums[i]  # takeaway what actually occurs
+
+        return res
