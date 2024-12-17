72c886231f8b4696f6c40be5b16cca0510ba2d6e
tobycourtis <toby.courtis@hotmail.com>
2024-12-17 16:13:22 +0000

    ✨ Practice another blind75, question 121

---
+++ leetcode/practice-blind75-2/121-best-time-buy-stock.py
@@ -0,0 +1,21 @@
+from typing import List
+
+
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        profit = 0
+        buy = prices[0]
+
+        for i in range(1, len(prices)):
+            sell = prices[i]
+            if sell - buy < 0:
+                buy = sell
+            else:
+                profit = max(profit, sell - buy)
+
+        return profit
+
+
+s = Solution()
+print(s.maxProfit([7, 1, 5, 3, 6, 4]))
+print(s.maxProfit([7, 6, 4, 3, 1]))
